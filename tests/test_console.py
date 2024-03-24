9cf0b96c57ec9609413f5b8ec114d6135f3eed3e
Youssef-faradi <faradiyoussef1@gmail.com>
2024-03-24 14:28:05 +0000

    build tests folder

---
+++ tests/test_console.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" A module that contains tests for console module """
+import unittest
+
+
+class tests_Console(unittest.TestCase):
+    """ A class for testing the HBNBCommand class """
+    pass
